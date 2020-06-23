d808ad6d75188e882950bdcb06517445e015dc05
Lisa <Lisa@lisa.comm>
2020-06-23 19:47:42 +0800

    Updates

---
+++ precache-manifest.99cf88b22b9bc5b3763784109768315d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a83e106e14d2a886419d3de6687e81",
+    "url": "/test-pages/index.html"
+  },
+  {
+    "revision": "c56124c8a8ac5c9e6e7a",
+    "url": "/test-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7498b0fade7bdf140e09",
+    "url": "/test-pages/static/js/2.b6290089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-pages/static/js/2.b6290089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56124c8a8ac5c9e6e7a",
+    "url": "/test-pages/static/js/main.4c9dce16.chunk.js"
+  },
+  {
+    "revision": "790164d759502a751b9e",
+    "url": "/test-pages/static/js/runtime-main.229fa0f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
